99320272d7366e85d5e322a7dbfc047dbba2e28a
DavAVP <davidvilanezpalma@hotmail.com>
2025-11-26 08:32:40 -0500

    cambios menores en el backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const app = express()
 const path = require('path')
 
-/* const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:4173'; */
 const allowedOrigins = [
     'http://localhost:4173',
     'http://localhost:5173',
@@ -13,7 +12,7 @@ const allowedOrigins = [
     process.env.FRONTEND_URL
 ].filter(Boolean);
 app.use(cors({
-    /* origin: [allowedOrigin], */
+
     origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
@@ -25,13 +24,6 @@ app.use(express.json())
 
 // Routes - rutas del servidor 
 app.use(require('./routes/index'))
-
-// Static Content
-const frontendPath = path.join(__dirname, '../dist');
-app.use(express.static(frontendPath));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'));
-});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
